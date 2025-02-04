sukibaby/itgmania-sukibaby

Fixed buffer size for RageSoundMixBuffer.
Pre-define a set of audio mix buffer sizes, and let the user choose. By extension, maintain a constant amount of latency, which is greatly beneficial for sync stability. We have known `RageSoundMixBuffer::Extend` is directly responsible for sync stability. The solution is to introduce a new preference to define a fixed audio buffer size. See the PR for more in-depth commentary on these changes.

src/RageSoundMixBuffer.cpp
@@ -1,50 +1,43 @@
 #include "global.h"
 #include "RageSoundMixBuffer.h"
 #include "RageUtil.h"
+#include "PrefsManager.h"
+#include "RageLog.h"
 
-#include <cmath>
 #include <cstdint>
 #include <cstdlib>
 
-RageSoundMixBuffer::RageSoundMixBuffer()
-{
-	m_pMixbuf = static_cast<float*>(std::malloc(BUF_SIZE * sizeof(float)));
-	if (m_pMixbuf == nullptr)
-	{
-		ASSERT_M(false, "Failed to allocate memory for the sound mixing buffer");
+static size_t size_in_bytes_ = 70560;
+
+static void GetBufferSizeFromPreference() {
+	size_in_bytes_ = static_cast<size_t>(PREFSMAN->m_RSMBBufferSize.Get());
+	if (size_in_bytes_ < 4409) {
+		LOG->Warn("The value of AudioBufferSize is too small (%zu) - changing the buffer size to 4410.", size_in_bytes_);
+		size_in_bytes_ = 4410;
 	}
-	m_iBufSize = BUF_SIZE;
-	std::memset(m_pMixbuf, 0, m_iBufSize * sizeof(float));
-	m_iBufUsed = 0;
-	m_iOffset = 0;
 }
 
+RageSoundMixBuffer::RageSoundMixBuffer() {
+	GetBufferSizeFromPreference();
+	LOG->Info("Audio mix buffer size: %.1f seconds @ 44.1kHz (%zu bytes)", static_cast<float>(size_in_bytes_) / (44100.0f * sizeof(float)), size_in_bytes_);
+	m_iBufSize = size_in_bytes_;
+	m_iBufUsed = 0;
+	m_iOffset = 0;
+	m_pMixbuf = static_cast<float*>(std::malloc(size_in_bytes_));
+	if (!(m_pMixbuf)) {
+		FailWithMessage("Failed to allocate memory for the audio mix buffer");
+	}
+	std::memset(m_pMixbuf, 0, size_in_bytes_);
+}
 
-RageSoundMixBuffer::~RageSoundMixBuffer()
-{
+RageSoundMixBuffer::~RageSoundMixBuffer() {
 	std::free(m_pMixbuf);
 }
 
-/* write() will start mixing iOffset samples into the buffer.  Be careful; this is
- * measured in samples, not frames, so if the data is stereo, multiply by two. */
-void RageSoundMixBuffer::SetWriteOffset( int iOffset )
-{
-	m_iOffset = iOffset;
-}
+void RageSoundMixBuffer::Extend(unsigned iSamples) noexcept {
+	const int_fast64_t realsize = iSamples + m_iOffset;
 
-void RageSoundMixBuffer::Extend(unsigned iSamples)
-{
-	const uint64_t realsize = static_cast<uint64_t>(iSamples) + static_cast<uint64_t>(m_iOffset);
-	if( m_iBufSize < realsize )
-	{
-		m_pMixbuf = static_cast<float*>(std::realloc(m_pMixbuf, sizeof(float) * realsize));
-		if (m_pMixbuf == nullptr)
-		{
-			ASSERT_M(false, "Failed to re-allocate memory for the sound mixing buffer.");
-		}
-		m_iBufSize = realsize;
-	}
-
+	// Initialize any unused part of the buffer with zeroes, if it exists.
 	if( m_iBufUsed < realsize )
 	{
 		std::memset(m_pMixbuf + m_iBufUsed, 0, (realsize - m_iBufUsed) * sizeof(float));
@@ -52,10 +45,10 @@ void RageSoundMixBuffer::Extend(unsigned iSamples)
 	}
 }
 
-void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceStride, int iDestStride )
-{
-	if( iSize == 0 )
+void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceStride, int iDestStride ) noexcept {
+	if( iSize == 0 ) {
 		return;
+	}
 
 	// iSize = 3, iDestStride = 2 uses 4 frames.  Don't allocate the stride of the last sample.
 	Extend( iSize * iDestStride - (iDestStride-1) );
@@ -63,8 +56,7 @@ void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceSt
 	// Scale volume and add.
 	float *pDestBuf = m_pMixbuf+m_iOffset;
 
-	while( iSize )
-	{
+	while( iSize ) {
 		*pDestBuf += *pBuf;
 		pBuf += iSourceStride;
 		pDestBuf += iDestStride;
@@ -72,30 +64,40 @@ void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceSt
 	}
 }
 
-void RageSoundMixBuffer::read( int16_t *pBuf )
-{
-	for( unsigned iPos = 0; iPos < m_iBufUsed; ++iPos )
-	{
-		float iOut = m_pMixbuf[iPos];
-		iOut = std::clamp( iOut, -1.0f, +1.0f );
-		pBuf[iPos] = static_cast<int>((iOut * 32767) + 0.5);
+/*
+ * Example usage:
+ * rsmb.Reinitialize(); // No argument, calls GetBufferSizeFromPreference()
+ * rsmb.Reinitialize(17640); // With arugment, updates size_in_bytes_ to be 17640
+ */
+void RageSoundMixBuffer::Reinitialize(unsigned new_size = 0) {
+	// Check if we were given a buffer size
+	size_in_bytes_ = (new_size > 0) ? new_size : size_in_bytes_;
+	if (new_size == 0) { GetBufferSizeFromPreference(); }
+	// Set up the temporary buffer, switch, & free old memory
+	float* temp_buf = static_cast<float*>(std::malloc(size_in_bytes_));
+	if (!(temp_buf)) {
+		FailWithMessage("Failed to allocate memory for the audio mix buffer");
+	}
+	std::memset(temp_buf, 0, size_in_bytes_);
+	float* old_buf = m_pMixbuf;
+	m_pMixbuf = temp_buf;
+	m_iBufSize = size_in_bytes_;
+	m_iBufUsed = 0;
+	m_iOffset = 0;
+	std::free(old_buf);
+	LOG->Info("Audio mix buffer size: %.1f seconds @ 44.1kHz (%zu bytes)", static_cast<float>(size_in_bytes_) / (44100.0f * sizeof(float)), size_in_bytes_);
+}
+
+void RageSoundMixBuffer::read_deinterlace( float **pBufs, int channels ) noexcept {
+	for( unsigned i = 0; i < m_iBufUsed / channels; ++i ) {
+		for( int ch = 0; ch < channels; ++ch ) {
+			pBufs[ch][i] = m_pMixbuf[channels * i + ch];
+		}
 	}
 	m_iBufUsed = 0;
 }
 
-void RageSoundMixBuffer::read( float *pBuf )
-{
-	std::memcpy( pBuf, m_pMixbuf, m_iBufUsed * sizeof(float) );
-	m_iBufUsed = 0;
-}
-
-void RageSoundMixBuffer::read_deinterlace( float **pBufs, int channels )
-{
-	for( unsigned i = 0; i < m_iBufUsed / channels; ++i )
-		for( int ch = 0; ch < channels; ++ch )
-			pBufs[ch][i] = m_pMixbuf[channels * i + ch];
-	m_iBufUsed = 0;
-}
+/* New code by sukibaby, 2024 - rewritten for fixed buffer size. */
 
 /*
  * Copyright (c) 2002-2004 Glenn Maynard

itgmania/ITGmania

src/RageSoundMixBuffer.cpp
@@ -1,49 +1,42 @@
 #include "global.h"
 #include "RageSoundMixBuffer.h"
 #include "RageUtil.h"
+#include "PrefsManager.h"
+#include "RageLog.h"
 
-#include <cmath>
 #include <cstdint>
 #include <cstdlib>
 
-RageSoundMixBuffer::RageSoundMixBuffer()
-{
-	m_pMixbuf = static_cast<float*>(std::malloc(BUF_SIZE * sizeof(float)));
-	if (m_pMixbuf == nullptr)
-	{
-		ASSERT_M(false, "Failed to allocate memory for the sound mixing buffer");
+static size_t size_in_bytes_ = 70560;
+
+static void GetBufferSizeFromPreference() {
+	size_in_bytes_ = static_cast<size_t>(PREFSMAN->m_AudioBufferSize.Get());
+	if (size_in_bytes_ < 4409) {
+		LOG->Warn("The value of AudioBufferSize is too small (%zu) - changing the buffer size to 4410.", size_in_bytes_);
+		size_in_bytes_ = 4410;
 	}
-	m_iBufSize = BUF_SIZE;
-	std::memset(m_pMixbuf, 0, m_iBufSize * sizeof(float));
-	m_iBufUsed = 0;
-	m_iOffset = 0;
 }
 
+RageSoundMixBuffer::RageSoundMixBuffer() {
+	GetBufferSizeFromPreference();
+	LOG->Info("Audio mix buffer size: %.1f seconds @ 44.1kHz (%zu bytes)", static_cast<float>(size_in_bytes_) / (44100.0f * sizeof(float)), size_in_bytes_);
+	m_iBufSize = size_in_bytes_;
+	m_iBufUsed = 0;
+	m_iOffset = 0;
+	m_pMixbuf = static_cast<float*>(std::malloc(size_in_bytes_));
+	if (!(m_pMixbuf)) {
+		FAIL_M("Failed to allocate memory for the audio mix buffer");
+	}
+	std::memset(m_pMixbuf, 0, size_in_bytes_);
+}
 
-RageSoundMixBuffer::~RageSoundMixBuffer()
-{
+RageSoundMixBuffer::~RageSoundMixBuffer() {
 	std::free(m_pMixbuf);
 }
 
-/* write() will start mixing iOffset samples into the buffer.  Be careful; this is
- * measured in samples, not frames, so if the data is stereo, multiply by two. */
-void RageSoundMixBuffer::SetWriteOffset( int iOffset )
-{
-	m_iOffset = iOffset;
-}
-
-void RageSoundMixBuffer::Extend(unsigned iSamples)
-{
-	const uint64_t realsize = static_cast<uint64_t>(iSamples) + static_cast<uint64_t>(m_iOffset);
-	if( m_iBufSize < realsize )
-	{
-		m_pMixbuf = static_cast<float*>(std::realloc(m_pMixbuf, sizeof(float) * realsize));
-		if (m_pMixbuf == nullptr)
-		{
-			ASSERT_M(false, "Failed to re-allocate memory for the sound mixing buffer.");
-		}
-		m_iBufSize = realsize;
-	}
+// Initialize any unused part of the buffer with zeroes, if it exists.
+void RageSoundMixBuffer::Extend(unsigned iSamples) noexcept {
+	const int_fast64_t realsize = iSamples + m_iOffset;
 
 	if( m_iBufUsed < realsize )
 	{
@@ -52,10 +45,10 @@ void RageSoundMixBuffer::Extend(unsigned iSamples)
 	}
 }
 
-void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceStride, int iDestStride )
-{
-	if( iSize == 0 )
+void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceStride, int iDestStride ) noexcept {
+	if( iSize == 0 ) {
 		return;
+	}
 
 	// iSize = 3, iDestStride = 2 uses 4 frames.  Don't allocate the stride of the last sample.
 	Extend( iSize * iDestStride - (iDestStride-1) );
@@ -63,8 +56,7 @@ void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceSt
 	// Scale volume and add.
 	float *pDestBuf = m_pMixbuf+m_iOffset;
 
-	while( iSize )
-	{
+	while( iSize ) {
 		*pDestBuf += *pBuf;
 		pBuf += iSourceStride;
 		pDestBuf += iDestStride;
@@ -72,30 +64,40 @@ void RageSoundMixBuffer::write( const float *pBuf, unsigned iSize, int iSourceSt
 	}
 }
 
-void RageSoundMixBuffer::read( int16_t *pBuf )
-{
-	for( unsigned iPos = 0; iPos < m_iBufUsed; ++iPos )
-	{
-		float iOut = m_pMixbuf[iPos];
-		iOut = std::clamp( iOut, -1.0f, +1.0f );
-		pBuf[iPos] = static_cast<int>((iOut * 32767) + 0.5);
+/*
+ * Example usage:
+ * rsmb.Reinitialize(); // No argument, calls GetBufferSizeFromPreference()
+ * rsmb.Reinitialize(17640); // With arugment, updates size_in_bytes_ to be 17640
+ */
+void RageSoundMixBuffer::Reinitialize(unsigned new_size = 0) {
+	// Check if we were given a buffer size
+	size_in_bytes_ = (new_size > 0) ? new_size : size_in_bytes_;
+	if (new_size == 0) { GetBufferSizeFromPreference(); }
+	// Set up the temporary buffer, switch, & free old memory
+	float* temp_buf = static_cast<float*>(std::malloc(size_in_bytes_));
+	if (!(temp_buf)) {
+		FAIL_M("Failed to allocate memory for the audio mix buffer");
+	}
+	std::memset(temp_buf, 0, size_in_bytes_);
+	float* old_buf = m_pMixbuf;
+	m_pMixbuf = temp_buf;
+	m_iBufSize = size_in_bytes_;
+	m_iBufUsed = 0;
+	m_iOffset = 0;
+	std::free(old_buf);
+	LOG->Info("Audio mix buffer size: %.1f seconds @ 44.1kHz (%zu bytes)", static_cast<float>(size_in_bytes_) / (44100.0f * sizeof(float)), size_in_bytes_);
+}
+
+void RageSoundMixBuffer::read_deinterlace( float **pBufs, int channels ) noexcept {
+	for( unsigned i = 0; i < m_iBufUsed / channels; ++i ) {
+		for( int ch = 0; ch < channels; ++ch ) {
+			pBufs[ch][i] = m_pMixbuf[channels * i + ch];
+		}
 	}
 	m_iBufUsed = 0;
 }
 
-void RageSoundMixBuffer::read( float *pBuf )
-{
-	std::memcpy( pBuf, m_pMixbuf, m_iBufUsed * sizeof(float) );
-	m_iBufUsed = 0;
-}
-
-void RageSoundMixBuffer::read_deinterlace( float **pBufs, int channels )
-{
-	for( unsigned i = 0; i < m_iBufUsed / channels; ++i )
-		for( int ch = 0; ch < channels; ++ch )
-			pBufs[ch][i] = m_pMixbuf[channels * i + ch];
-	m_iBufUsed = 0;
-}
+/* New code by sukibaby, 2024 - rewritten for fixed buffer size. */
 
 /*
  * Copyright (c) 2002-2004 Glenn Maynard